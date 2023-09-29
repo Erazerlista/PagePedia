745cfcaa7ebfaafe79abbdc4f678fc588ee0f632
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-29 15:47:16 -0700

    installs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create an Apollo Server instance
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply Apollo Server as middleware to Express app
+apolloServer.applyMiddleware({ app });
+
+// Middleware to parse JSON and URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
