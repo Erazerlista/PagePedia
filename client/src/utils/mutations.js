a4d002cf4d1df8ff1f9891b58dff807b48e786a4
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-29 19:04:44 -0700

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from 'graphql-tag';
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
-    login($email: $email, password: $password) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
@@ -12,23 +12,48 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email password: $password) {
+      token
+      user {
       _id
-      name
-      skills
+      username
     }
   }
 `;
 
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      profile {
-        _id
-        name
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $description: String!, &title: String!, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $ bookId, image: $image link: $link) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        title
+        link
+      }
+    }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $ bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        title
+        link
       }
     }
   }
